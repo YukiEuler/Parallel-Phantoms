1061b90e20ada54ab6be583ede142603c7b91858
YukiSaki <yesayasusanto136@gmail.com>
2024-02-01 18:51:20 +0700

    feat: Add Kuntilanak

--- Enemy/Kuntilanak/Script/Kuntilanak.gd
+++ Enemy/Kuntilanak/Script/Kuntilanak.gd
@@ -66,7 +66,7 @@ func model_rotation(delta):
 
 func _on_player_body_entered(body):
 	if body.is_in_group("Player"):
-		print("H-hmphh")s
+		print("H-hmphh")
 		if not (player.SPEED == player.BLOCKING_SPEED):
 			body.health -= 1
 		player.knock_dir = direction
